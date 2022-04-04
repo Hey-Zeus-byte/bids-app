294493a2f2cce543363ac4ff90534cc891a2c25b
Hey-Zeus-byte <heyzeus.1096@gmail.com>
2022-04-04 08:15:58 -0700

    remove2

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-<<<<<<< HEAD
-// import App from "./App";
-=======
->>>>>>> 1ed061647d70da753ed891d3b4061b97b488cc8a
+
 
 ReactDOM.render(
   <React.StrictMode>
